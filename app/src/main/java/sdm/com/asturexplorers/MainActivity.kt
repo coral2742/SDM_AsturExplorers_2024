740ea928cfbbc9f064f68c6ded2acc18c49f0032
uo284247 <uo284247@uniovi.es>
2024-11-04 23:07:45 +0100

    recycler hecho

--- app/src/main/java/sdm/com/asturexplorers/MainActivity.kt
+++ app/src/main/java/sdm/com/asturexplorers/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var rutas: MutableList<Ruta>
+    private lateinit var adapter: RutasAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        inicializar()
+        rutas = MutableList(30) { i -> Ruta (
+            id = i,
+            nombre = "Ruta del Cares",
+            distancia = 21.8,
+            dificultad = "Media",
+            tipoRecorrido = "A pie",
+            descripcion = "Ruta muy mítica",
+            "https://www.nachocuesta.com/sh/content/img/gal/2082/ruta-del-cares-vii_201905071522485cd1a2c85711b.sized.jpg")}
+        adapter = RutasAdapter(rutas)
+        recyclerView.adapter = adapter
+
+    }
+
+    private fun inicializar() {
+        recyclerView = findViewById(R.id.recycler)
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/sdm/com/asturexplorers/RutasAdapter.kt
@@ -0,0 +1,57 @@
+package sdm.com.asturexplorers
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+
+
+class RutasAdapter(
+    private val listaRutas: List<Ruta>
+) : RecyclerView.Adapter<RutasAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RutasAdapter.ViewHolder {
+        val layoutElement = R.layout.card_ruta
+        val view = LayoutInflater.from(parent.context).inflate(layoutElement, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listaRutas[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listaRutas.size
+    }
+
+    class ViewHolder(
+        view: View
+    ) : RecyclerView.ViewHolder(view) {
+
+        private val tvTitulo: TextView = view.findViewById(R.id.tvTitulo)
+        private val tvDificultad: TextView = view.findViewById(R.id.tvDificultadReal)
+        private val tvDistancia: TextView = view.findViewById(R.id.tvDistanciaReal)
+        private val tvRecorrido: TextView = view.findViewById(R.id.tvRecorridoReal)
+        private val imageView: ImageView = view.findViewById((R.id.cardImage))
+        private var rutaActual: Ruta? = null
+
+        init {
+
+        }
+        @SuppressLint("SetTextI18n")
+        fun bind(ruta : Ruta) {
+            rutaActual = ruta
+            tvTitulo.text = ruta.nombre
+            tvDificultad.text = ruta.dificultad
+            tvRecorrido.text = ruta.tipoRecorrido
+            tvDistancia.text = ruta.distancia.toString() + " km"
+            imageView.load(ruta.imagenUrl)
+
+        }
+    }
+
+}

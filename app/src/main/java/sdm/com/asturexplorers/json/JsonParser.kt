0d9fc08c39d27049be74e6a0d59d5d3233cf76bc
uo284247 <uo284247@uniovi.es>
2024-11-17 14:30:09 +0100

    refactorización de código

--- app/src/main/java/sdm/com/asturexplorers/json/JsonParser.kt
+++ app/src/main/java/sdm/com/asturexplorers/json/JsonParser.kt
@@ -1,7 +1,6 @@
 package sdm.com.asturexplorers.json
 
 import com.google.gson.Gson
-import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import sdm.com.asturexplorers.db.Ruta
